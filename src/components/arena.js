7930cfb6cbfaaf72d369b76c924970d6e7b19cb0
Garrett Vorbeck <gvorbeck@gmail.com>
2022-04-19 11:59:29 -0400

    resorting fields based on initiative/name

--- src/components/arena.js
+++ src/components/arena.js
@@ -1,19 +1,108 @@
 import React from 'react';
 import {
-  Box, Drawer, FormControl, Input, InputLabel, List, ListItem, SvgIcon, TextField,
+  Box, Drawer, FormControl, Input, InputLabel, List, ListItem, ListItemText, SvgIcon, TextField,
 } from '@mui/material';
 import { IconButton } from 'gatsby-theme-material-ui';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import CasinoIcon from '@mui/icons-material/Casino';
 
 function useArena() {
-  // Drawer State.
+  // Prep Session Storage data:
+  const ssKey = 'gmcm-combatants';
+  const parsedSessionData = JSON.parse(sessionStorage.getItem(ssKey) || '[]');
+  if (parsedSessionData.length > 0) {
+    parsedSessionData.sort((a, b) => (a.initiative < b.initiative ? 1 : -1));
+    sessionStorage.setItem(ssKey, JSON.stringify(parsedSessionData));
+  }
+  // PARSED data:
+  const [arenaSessionStorage, setArenaSessionStorage] = React.useState(parsedSessionData);
+
+  // Drawer State:
   const [arenaDrawerOpen, setArenaDrawerOpen] = React.useState(false);
-  // Parsed Session Storage data.
-  const [arenaSessionStorage, setArenaSessionStorage] = React.useState(
-    JSON.parse(sessionStorage.getItem('gmcm-combatants') || '[]'),
-  );
+
+  // For manual entry, Player Name:
   const [playerName, setPlayerName] = React.useState('');
+
+  // Create ListItems
+  const [combatantListItems, setCombatantListItems] = React.useState([]);
+  // Submit new initiative by making a shallow copy of data, manipulating it, and then
+  // submiting the new data.
+  const initiativeSubmit = (value, index) => {
+    const arenaCopy = [...arenaSessionStorage];
+    const arenaCopyItem = { ...arenaCopy[index] };
+    arenaCopyItem.initiative = parseInt(value.replace(/\D/g, ''), 10);
+    arenaCopy[index] = arenaCopyItem;
+    setArenaSessionStorage(arenaCopy);
+  };
+  const combatantListMaker = (array) => array
+    // Sort by initiative and then by name.
+    .sort(
+      (a, b) => (
+        (a.initiative < b.initiative) ? 1 : (a.initiative === b.initiative) ? (
+          (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1
+        ) : -1
+      ),
+    )
+    .map((item, index) => (
+      <ListItem key={`${item.name}-${Math.random()}`}>
+        {/* Initiative value */}
+        <TextField
+          variant="standard"
+          defaultValue={item.initiative}
+          onBlur={(event) => initiativeSubmit(event.target.value, index)}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              event.target.blur();
+            }
+          }}
+        />
+        {/* Combatant's name */}
+        <ListItemText primary={item.name} />
+        {/* HP value */}
+        <TextField
+          variant="standard"
+          defaultValue={item.hp || 0}
+        />
+      </ListItem>
+    ));
+
+  // IF arenaSessionStorage value is altered, reset the Session Storage data:
+  React.useEffect(() => {
+    console.log('arenaSessionStorage useEffect');
+    // Resort the data and recreate the list.
+    setCombatantListItems(combatantListMaker(arenaSessionStorage));
+    // Stringify and resend the data to Session Storage.
+    sessionStorage.setItem(ssKey, JSON.stringify(arenaSessionStorage));
+  }, [arenaSessionStorage]);
+
+  return {
+    arenaSessionStorage,
+    setArenaSessionStorage,
+    arenaDrawerOpen,
+    setArenaDrawerOpen,
+    arenaRender: (
+      <Drawer
+        anchor="right"
+        open={arenaDrawerOpen}
+        onClose={() => setArenaDrawerOpen(false)}
+        sx={{
+          p: '1rem',
+        }}
+      >
+        <List>{combatantListItems}</List>
+      </Drawer>
+    ),
+  };
+}
+
+{ /* function useArena() {
+  // Drawer State.
+  // const [arenaDrawerOpen, setArenaDrawerOpen] = React.useState(false);
+  // Parsed Session Storage data.
+  // const [arenaSessionStorage, setArenaSessionStorage] = React.useState(
+  //   JSON.parse(sessionStorage.getItem('gmcm-combatants') || '[]'),
+  // );
+  // const [playerName, setPlayerName] = React.useState('');
   const [error, setError] = React.useState(false);
   // Mapping ListItems based on parsed Session Storage data
   const combatantListItems = arenaSessionStorage
@@ -51,9 +140,9 @@ function useArena() {
       );
     });
   // Set Stringified Session Storage data.
-  React.useEffect(() => {
-    sessionStorage.setItem('gmcm-combatants', JSON.stringify(arenaSessionStorage));
-  }, [arenaSessionStorage]);
+  // React.useEffect(() => {
+  //   sessionStorage.setItem('gmcm-combatants', JSON.stringify(arenaSessionStorage));
+  // }, [arenaSessionStorage]);
 
   return {
     arenaSessionStorage,
@@ -101,6 +190,6 @@ function useArena() {
       </Drawer>
     ),
   };
-}
+} */ }
 
 export default useArena;
