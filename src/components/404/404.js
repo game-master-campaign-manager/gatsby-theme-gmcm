7861efbde81858f3569a3aeebccdfc8c80e2c128
JGV <jgv@JGVs-MacBook-Air.local>
2022-05-01 11:36:22 -0400

    404 strings

---
+++ src/components/404/404.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Layout from '../layout';
+
+function FourOhFour({ data }) {
+  return (
+    <Layout title={data.title}>
+      <Box>
+        <Paper sx={{ p: 2, textAlign: 'center' }}>
+          <Image image={data.image} />
+          <Divider sx={{ my: 2 }} />
+          {data.text.map((t) => (
+            <Typography key={t}>{t}</Typography>
+          ))}
+        </Paper>
+      </Box>
+    </Layout>
+  );
+}
+
+function Image({ image }) {
+  return <StaticImage src="../../images/404.png" alt={image} />;
+}
+
+export default FourOhFour;
---
+++ src/components/404/404Strings.js
@@ -0,0 +1,7 @@
+const fourOhFourStrings = {
+  title: '404!',
+  text: ['Your party has become lost in rough terrain.', 'Turn back now.'],
+  image: 'Sword',
+};
+
+export default fourOhFourStrings;
