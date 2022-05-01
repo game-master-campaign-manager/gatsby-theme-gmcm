f886fd2030c4e66486dfda6cce4f6f2d53700fc2
JGV <jgv@JGVs-MacBook-Air.local>
2022-05-01 11:36:48 -0400

    location page layout strings

---
+++ src/components/LocationPageLayout/locationPageLayoutStrings.js
@@ -0,0 +1,23 @@
+const locationPageLayoutStrings = {
+  navigation: {
+    map: 'map',
+    general: 'General Information',
+    directions: {
+      up: 'up',
+      down: 'down',
+    },
+  },
+  flags: {
+    unknown: 'The flag listed in this location\'s frontmatter is unrecognized.',
+    person: 'A person of interest is here.',
+    monster: 'Monster(s) present here.',
+    secret: 'There are hidden passages or items here.',
+    eyes: 'Players can/are being observed here.',
+    treasure: 'There is loot here.',
+    trap: 'There is a trap here.',
+    boss: 'A boss level monster is here.',
+  },
+
+};
+
+export default locationPageLayoutStrings;
