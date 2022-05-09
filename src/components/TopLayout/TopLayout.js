532e234a7f57c047c3510f8da695a34ea4763af5
JGV <jgv@JGVs-MacBook-Air.local>
2022-05-09 01:13:35 -0400

    i dont think i need this

--- src/components/TopLayout/TopLayout.js
+++ src/components/TopLayout/TopLayout.js
@@ -10,7 +10,7 @@ export default function TopLayout({ children }) {
       <Helmet>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" /> */}
       </Helmet>
       <ThemeProvider theme={theme}>
         {/* CSS Reset */}
