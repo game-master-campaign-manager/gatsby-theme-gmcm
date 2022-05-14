6496b53f53a3f2f6a87dd14ffc1c0e78cb452002
JGV <jgv@JGVs-MacBook-Air.local>
2022-05-14 09:53:00 -0400

    speeddial actions

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,11 @@
+import React from 'react';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+
 export const SCROLL_BEHAVIOR = {
   behavior: 'smooth',
   block: 'start',
 };
+
+export const SPEEDDIAL_ACTION = [
+  { icon: <ArrowUpwardIcon />, name: 'Scroll' },
+];
